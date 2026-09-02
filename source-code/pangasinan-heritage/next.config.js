ff6a44ddd3a258b3161c888eb73f95bd1b8c33cd
Maurik Fernandez <maurikleano@gmail.com>
2026-09-02 23:34:03 +0800

    Fix GitHub Pages asset paths

--- source-code/pangasinan-heritage/next.config.js
+++ source-code/pangasinan-heritage/next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/pangasinan-heritage" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/pangasinan-heritage/" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/pangasinanheritageact" : "",
   env: {
-    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === "production" ? "/pangasinan-heritage" : ""
+    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === "production" ? "/pangasinanheritageact" : ""
   },
   images: {
     unoptimized: true
