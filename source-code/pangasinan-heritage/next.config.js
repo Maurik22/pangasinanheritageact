b02568db0467ea6720784905a077dbb65b701453
Maurik Fernandez <maurikleano@gmail.com>
2026-09-02 22:21:43 +0800

    Complete Activity 2 project

--- source-code/pangasinan-heritage/next.config.js
+++ source-code/pangasinan-heritage/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/mau" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/mau/" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/pangasinanheritageact" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/pangasinanheritageact/" : "",
   env: {
-    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === "production" ? "/mau" : ""
+    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === "production" ? "/pangasinanheritageact" : ""
   },
   images: {
     unoptimized: true
